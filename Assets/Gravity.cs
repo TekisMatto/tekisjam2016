f7d8497b169d0e1c4f5b45a9ebd14a272cc767cf
Rasmus <rastavi@helsinki.fi>
2016-11-18 22:03:13 +0200

    initial physics

--- Assets/Gravity.cs
+++ Assets/Gravity.cs
@@ -2,14 +2,19 @@
 using System.Collections;
 
 public class Gravity : MonoBehaviour {
-
+	public float gravity;
+	private Rigidbody2D rb;
 	// Use this for initialization
-	void Start () {
-	
+	void Start() {
+		rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		Vector2 v = new Vector2 (0, gravity * Time.deltaTime);
+		rb.velocity -= v;
+
 	}
+
+
 }
