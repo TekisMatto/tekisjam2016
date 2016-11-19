ad94f78586ae510f5f7c7ac12ac94b5c27440fc7
jclc <johannes.lakkisto@helsinki.fi>
2016-11-19 17:35:39 +0200

    added rock spawner and despawner

---
+++ Assets/RockDespawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RockDespawner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter2D (Collider2D collider) {
+		if (collider.gameObject.tag == "Rock") {
+			Destroy (collider.gameObject);
+		}
+	}
+}
