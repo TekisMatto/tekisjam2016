ca8d9796acd90dafdfd8a65e1352bb449afefd4c
JuicyP <kinnunen.jussi@hotmail.com>
2016-11-19 19:25:57 +0200

    Player scripts

---
+++ Assets/Facing.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Facing : MonoBehaviour {
+
+    public bool isFacingRight;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Horizontal") > 0)
+            isFacingRight = true;
+        else if (Input.GetAxis("Horizontal") < 0)
+            isFacingRight = false;
+        GetComponentInChildren<SpriteRenderer>().flipX = !isFacingRight;
+    }
+}
