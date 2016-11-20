40d9c97f7e14ccbeab7f2867aef11fe60281070b
Rasmus <rastavi@helsinki.fi>
2016-11-20 15:05:10 +0200

    Hitstop component

---
+++ Assets/Hitstop.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class Hitstop : MonoBehaviour {
+
+	static bool isHitstop(GameObject go) {
+		Hitstop hs = go.GetComponent<Hitstop> ();
+		if (hs && hs.isHitstop ())
+			return true;
+		return false;
+	}
+
+	private float amount;
+
+	public bool isHitstop() {
+		return amount > 0;
+	}
+
+	public void addHitstop(float x) {
+		amount += x;
+	}
+
+	// Use this for initialization
+	void Start () {
+		amount = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if ( isHitstop() ) { // no simulation during hitsop
+			amount -= Time.deltaTime;
+			if (amount < 0)
+				amount = 0;
+			return;
+		}
+	}
+}
