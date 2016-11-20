0371d98df3d5a04d2ebe5804aed7ca64d420d281
Rasmus <rastavi@helsinki.fi>
2016-11-20 14:41:06 +0200

    Fix player prefab

--- Assets/Player/Physics.cs
+++ Assets/Player/Physics.cs
@@ -5,14 +5,22 @@ public class Physics : MonoBehaviour {
 	public float gravity;
 	private Rigidbody2D rb;
 	private Player player;
+
+	public float hitStop;
 	// Use this for initialization
 	void Start() {
 		rb = GetComponent<Rigidbody2D>();
 		player = GetComponent<Player> ();
+		hitStop = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (hitStop > 0) {
+			hitStop -= Time.deltaTime;
+			return;
+		}
 		
 		if (!player.isGrounded) {
 
