2bb9bfb73dd8cf2c8717f3274256ebf1435529db
Rasmus <rastavi@helsinki.fi>
2016-11-20 18:24:29 +0200

    final

---
+++ Assets/Attack - Copy/Attack.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attack : MonoBehaviour {
+
+
+    public GameObject attack;
+	private int player_id;
+
+    public float attack1CooldownTimer;
+    public float attack2CooldownTimer;
+
+    public float attack1ActiveTimer;
+    public float attack2ActiveTimer;
+
+    public float attack1StartupTimer;
+    public float attack2StartupTimer;
+
+    float runningTimer;
+
+    public bool isAttacking;
+
+    // Use this for initialization
+    void Start () {
+        isAttacking = false;
+		player_id = gameObject.GetComponent <Player> ().player_id;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Hitstop.isHitstop (gameObject))
+			return;
+
+        if (isAttacking)
+            runningTimer -= Time.deltaTime;
+
+        if (runningTimer < 0)
+            isAttacking = false;
+
+		if ((Input.GetButtonDown("Fire1_id" + player_id) || Input.GetButtonDown("Fire2_id" + player_id)) && !isAttacking) {
+            //float inputDirection = Input.GetAxis("Horizontal");
+            Vector2 playerMidPoint = (Vector2)GetComponent<Transform>().position;// + GetComponent<BoxCollider2D>().size / 2;
+            GameObject newHitbox = Instantiate(attack);
+
+            Vector2 offSet = new Vector2(0,0);
+
+            if (Input.GetButtonDown("Fire1_id" + player_id)) { // Strike. Able to set custom timers this way
+                Destroy(newHitbox, attack1ActiveTimer);
+                offSet = new Vector2(0.6f, 0);
+                newHitbox.GetComponent<Hitbox>().type = 1;
+                newHitbox.GetComponent<Hitbox>().startupTimer = attack1StartupTimer;
+                runningTimer = attack1CooldownTimer;
+            } else { // Bunt
+                Destroy(newHitbox, attack2ActiveTimer);
+                newHitbox.transform.Rotate(0, 0, 90);
+                offSet = new Vector2(0.5f, 0);
+                newHitbox.GetComponent<Hitbox>().type = 2;
+                newHitbox.GetComponent<Hitbox>().startupTimer = attack2StartupTimer;
+                runningTimer = attack2CooldownTimer;
+            }
+            
+
+
+            if (GetComponent<Facing>().isFacingRight) {
+                newHitbox.transform.position = new Vector2(offSet.x + playerMidPoint.x, offSet.y + playerMidPoint.y);
+            } else {
+                newHitbox.transform.position = new Vector2(-offSet.x + playerMidPoint.x, offSet.y + playerMidPoint.y);
+				if (Input.GetButtonDown("Fire1_id" + player_id)) // Cosmetic for current sprites
+                {
+                    newHitbox.GetComponent<SpriteRenderer>().flipX = false;
+                }
+                else
+                {
+                    newHitbox.GetComponent<SpriteRenderer>().flipX = false;
+                    newHitbox.GetComponent<SpriteRenderer>().flipY = true;
+                }
+            }
+
+            newHitbox.transform.SetParent(GetComponent<Transform>());
+            isAttacking = true;
+        }
+    }
+}
---
+++ Assets/Player - Copy/HealthBar.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour {
+
+	public int width;
+	public int offset;
+
+	private Rect remainingRect;
+	private Rect takenRect;
+	private int player_id;
+	private static Texture2D _staticRectTexture;
+	private static GUIStyle _staticRectStyle;
+	private static Color remainingColor = new Color(0.0F, 1.0F, 0.0F);
+	private static Color takenColor = new Color(1.0F, 0.0F, 0.0F);
+
+	// Use this for initialization
+	void Start () {
+		player_id = gameObject.GetComponent <Player> ().player_id;
+		remainingRect.y = 0;
+		takenRect.y = 0;
+		remainingRect.height = width;
+		takenRect.height = width;
+	}
+
+	void Update () {
+		UpdateRects ();
+	}
+
+	void OnGUI () {
+		GUIDrawRect (remainingRect, remainingColor);
+		GUIDrawRect (takenRect, takenColor);
+	}
+
+	void UpdateRects () {
+		float healthRatio =
+			(float) gameObject.GetComponent <Player> ().health / gameObject.GetComponent <Player> ().maxHealth;
+		Debug.Log (healthRatio);
+		remainingRect.x = 0 + (player_id == 1 ? 0 : (Screen.width / 2));
+		remainingRect.width = Screen.width / 2 * healthRatio;
+		takenRect.x = Screen.width / 2 * healthRatio + (player_id == 1 ? 0 : (Screen.width / 2));
+		takenRect.width = Screen.width / 2 * (1 - healthRatio);
+	}
+
+	// Note that this function is only meant to be called from OnGUI() functions.
+	public static void GUIDrawRect( Rect position, Color color )
+	{
+		if( _staticRectTexture == null )
+		{
+			_staticRectTexture = new Texture2D( 1, 1 );
+		}
+
+		if( _staticRectStyle == null )
+		{
+			_staticRectStyle = new GUIStyle();
+		}
+		_staticRectTexture.SetPixel( 0, 0, color );
+		_staticRectTexture.Apply();
+
+		_staticRectStyle.normal.background = _staticRectTexture;
+
+		GUI.Box( position, GUIContent.none, _staticRectStyle );
+	}
+}
