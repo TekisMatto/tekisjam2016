849546c5ca38331085d97148931041ef3bf0e213
JuicyP <kinnunen.jussi@hotmail.com>
2016-11-20 17:07:27 +0200

    implemented rock hitting player

--- Assets/Player/OnHitPlayer.cs
+++ Assets/Player/OnHitPlayer.cs
@@ -4,6 +4,10 @@ using System.Collections;
 public class OnHitPlayer : MonoBehaviour {
 
 	public GameObject victoryHandler;
+    public float rockSpeedThreshold;
+    public float minRockDamage;
+    public float rockDamageScale;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -14,6 +18,24 @@ public class OnHitPlayer : MonoBehaviour {
 	
 	}
 
+    public bool Damage (int damage)
+    {
+        int health = gameObject.GetComponent<Player>().health -= damage;
+        if (health <= 0)
+        {
+            int id = gameObject.GetComponent<Player>().player_id;
+            if (id == 1)
+                id = 2;
+            else
+                id = 1;
+            GameObject vh = Instantiate(victoryHandler);
+            vh.GetComponent<VictoryHandlerScript>().text = "Player " + id + " wins!";
+            Destroy(gameObject);
+            return true;
+        }
+        return false;
+    }
+
 	// When hit by another player
 	public void OnHit (GameObject hitter, int type) {
 		int damage;
@@ -23,17 +45,8 @@ public class OnHitPlayer : MonoBehaviour {
 			damage = 0;
 		}
 
-		int health = gameObject.GetComponent <Player> ().health -= damage;
-		if (health <= 0) {
-			int id = gameObject.GetComponent <Player> ().player_id;
-			if (id == 1)
-				id = 2;
-			else
-				id = 1;
-			GameObject vh = Instantiate (victoryHandler);
-			vh.GetComponent <VictoryHandlerScript> ().text = "Player " + id + " wins!";
-			Destroy (gameObject);
-		}
+        if (Damage(damage))
+            return;
 
 		bool facingRight = hitter.GetComponent<Facing>().isFacingRight;
 		Rigidbody2D rb = gameObject.GetComponent <Rigidbody2D>();
@@ -42,4 +55,21 @@ public class OnHitPlayer : MonoBehaviour {
 			4 + (type == 1 ? 6 : 0));
 
 	}
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Rock")
+        {
+            Vector2 rockVelocity = collider.gameObject.GetComponent<Rigidbody2D>().velocity;
+            if (rockVelocity.magnitude > rockSpeedThreshold)
+            {
+                int damage = (int)(minRockDamage + (rockVelocity.magnitude - rockSpeedThreshold) * rockDamageScale);
+                if (Damage(damage))
+                    return;
+
+                collider.gameObject.GetComponent<Rigidbody2D>().velocity /= 2;
+            }
+
+        }
+    }
 }
